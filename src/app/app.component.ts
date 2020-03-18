7376df3d748ac550d48377420499d2e4a1ef2831
04prashant <prashant.puri@nagarro.com>
2020-03-18 14:02:22 +0530

    Initial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos-app';
+
+  todos: any = ['a', 'b', 'c'];
+
+  searchValue = '';
+  addTodos(todo) {
+    this.todos.push(todo.value);
+    this.searchValue = '';
+  }
+  removeTodo( item, index) {
+    console.log(item);
+    console.log(index);
+    this.todos.splice(index, 1);
+  }
+  clearAll() {
+    console.log('Inside clear');
+    this.todos = [];
+  }
+  sort() {
+    this.todos.sort((a, b) => {
+    // if (a < b) { return -1; } else if (a > b) { return 1; } else {return 0; }
+    // descending order
+    return (b - a);
+  });
+  }
 }
